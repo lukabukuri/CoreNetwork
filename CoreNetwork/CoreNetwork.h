74229822caf9768f2cab50f7f31f69dce19e6773
Luka Bukuri <lukabukuri@gmail.com>
2022-10-26 13:07:16 +0400

    Initial Commit

---
+++ CoreNetwork/CoreNetwork.h
@@ -0,0 +1,18 @@
+//
+//  CoreNetwork.h
+//  CoreNetwork
+//
+//  Created by Luka Bukuri on 26.10.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreNetwork.
+FOUNDATION_EXPORT double CoreNetworkVersionNumber;
+
+//! Project version string for CoreNetwork.
+FOUNDATION_EXPORT const unsigned char CoreNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreNetwork/PublicHeader.h>
+
+
