c1879fffccea309147eaf5b3c9037a8fb7dc0d25
ace607 <mishkachargazia@gmail.com>
2022-11-03 13:46:34 +0400

    Added Endpoint model and refactored URLRequest init method

---
+++ CoreNetwork/CoreNetwork+Endpoint.swift
@@ -0,0 +1,107 @@
+//
+//  CoreNetwork+Endpoint.swift
+//  CoreNetwork
+//
+//  Created by Mishka Chargazia on 03.11.22.
+//
+
+import Foundation
+
+public extension CoreNetwork {
+    
+    /// Endpoint model for request
+    struct Endpoint {
+        /// Scheme
+        ///
+        /// - The scheme subcomponent of the URL
+        var scheme: Scheme = .defaultScheme
+        
+        /// Host
+        ///
+        /// - The host subcomponent of the URL
+        ///
+        /// - Note: Nullable
+        var host: String?
+        
+        /// Path
+        ///
+        /// - The path subcomponent of the URL
+        var path: String
+        
+        /// Query
+        ///
+        ///  - query URL component
+        var query: Query = .emptyQuery
+        
+        /// Method
+        ///
+        /// - The HTTP request method
+        var method: HTTPMethod = .get
+        
+        /// Headers
+        ///
+        /// - A dictionary containing the HTTP header fields for a request
+        var headers: Headers = .defaultHeaders
+        
+        /// Body
+        ///
+        /// - A dictionary of the data sent as the message body of a request, such as for an HTTP POST request
+        var body: Body = .emptyBody
+    }
+    
+}
+
+public extension CoreNetwork.Endpoint {
+    
+    /// URL Scheme subcomponent
+    enum Scheme {
+        /// HTTPS
+        ///
+        /// - String value: "https"
+        case https
+        /// HTTP
+        ///
+        /// - String value: "http"
+        case http
+        /// Custom scheme
+        ///
+        /// - String value returns the string given in the associated parameter
+        /// - Parameter value: Custom value for scheme subcomponent
+        case custom(value: String)
+        
+        /// Value
+        ///
+        /// - String value for scheme subcomponent
+        var value: String {
+            switch self {
+            case .https:
+                return "https"
+            case .http:
+                return "http"
+            case .custom(let value):
+                return value
+            }
+        }
+        
+        /// Default scheme
+        ///
+        /// - Set to https
+        static let defaultScheme: Self = .https
+    }
+    
+}
+
+public extension CoreNetwork.Endpoint {
+    
+    /// Creates URLComponents object from given subcomponents of endpoint
+    func urlComponents() -> URLComponents {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = scheme.value
+        urlComponents.path = path
+        urlComponents.host = host
+        urlComponents.queryItems = query.urlQueryItems()
+        
+        return urlComponents
+    }
+    
+}
--- CoreNetwork/Extensions/URLRequest.swift
+++ CoreNetwork/Extensions/URLRequest.swift
@@ -9,22 +9,43 @@ import Foundation
 
 extension URLRequest {
     
-    public init?(url: URL, method: CoreNetwork.HTTPMethod, headers: CoreNetwork.Headers? = nil, body: CoreNetwork.Body? = nil)  {
+    /// Creates and initializes a URL request with the given Endpoint model
+    ///
+    /// - Parameters:
+    ///    - endpoint: Endpoint model for the request
+    ///
+    /// - Throws:
+    ///     - badURL if URL can't be constructed using
+    ///
+    public init?(from endpoint: CoreNetwork.Endpoint) throws {
+        var urlComponents = endpoint.urlComponents()
+        guard let url = urlComponents.url else { throw CoreNetwork.Status.badURL }
+        
         self.init(url: url)
         
-        httpMethod = method.rawValue
-        
-        if let headers {
-            for (headerField, headerValue) in headers {
-                setValue(headerValue, forHTTPHeaderField: headerField)
-            }
+        set(headers: endpoint.headers)
+        try set(body: endpoint.body)
+    }
+    
+    /// Set HTTP headers from given endpoint
+    ///
+    /// - Note: Will replace values if any of the given header fields already exist
+    public mutating func set(headers: CoreNetwork.Headers) {
+        for (headerField, headerValue) in headers {
+            setValue(headerValue, forHTTPHeaderField: headerField)
         }
-        
-        if let body {
+    }
+    
+    /// Set HTTP body if exists
+    ///
+    /// - Throws:
+    ///     - encodingError if JSON data can't be serialized from given object
+    public mutating func set(body: CoreNetwork.Body) throws {
+        if !body.isEmpty {
             do {
                 httpBody = try JSONSerialization.data(withJSONObject: body)
             } catch {
-                return nil
+                throw CoreNetwork.Status.encodingError
             }
         }
     }
