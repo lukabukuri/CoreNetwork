5b4a6d40f841c38b411e3a80053056239b388c30
Luka Bukuri <lukabukuri@gmail.com>
2022-12-07 02:38:42 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "CoreNetwork",
-    platforms: [
-        .iOS(.v13),
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
