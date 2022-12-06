2e9e0fe5d8575eb227ba3de916021a0d637d4f30
Luka Bukuri <lukabukuri@gmail.com>
2022-12-07 02:00:32 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CoreNetwork",
-            platforms: [.iOS("13.0")],
             targets: ["CoreNetwork"]),
     ],
     dependencies: [
